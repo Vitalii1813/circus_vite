5ddcc462ebcf65beb695e92c327dc811017ef21b
Vitalii1813 <96308455+Vitalii1813@users.noreply.github.com>
2024-09-12 20:25:13 -0700

    changes

--- App.tsx
+++ App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './ios/src/components/HomeScreen';
-import BookingScreen from './ios/src/components/BookingScreen';
-import SouvenirsScreen from './ios/src/components/SouvenirScreen';
-import ScheduleScreen from './ios//src/components/ScheduleScreen';
-import ProfileScreen from './ios/src/components/ProfileScreen';
+import BookingScreen from './src/components/BookingScreen';
+import ScheduleScreen from './src/components/ScheduleScreen';
+import ProfileScreen from './src/components/ProfileScreen';
+import HomeScreen from './src/components/HomeScreen';
+import SouvenirScreen from './src/components/SouvenirScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown  : false}}>
+        <Stack.Screen name="Home" component={TabStumble} />
         <Stack.Screen name="Booking" component={BookingScreen} />
-        <Stack.Screen name="Souvenirs" component={SouvenirsScreen} />
+        <Stack.Screen name="Souvenirs" component={SouvenirScreen} />
         <Stack.Screen name="Schedule" component={ScheduleScreen} />
         <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
+  );
+}
+
+function TabStumble(){
+  return (
+    <Tab.Navigator screenOptions={{headerShown  : false}}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={ProfileScreen} />
+    </Tab.Navigator>
   );
 }
---
+++ src/components/SouvenirScreen.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+export default function SouvenirScreen() {
+  return (
+    
+    <View>
+      <SafeAreaView/>
+      <Text>Souvenir Catalog</Text>
+      {/* Пізніше додати список сувенірів */}
+    </View>
+  );
+}
